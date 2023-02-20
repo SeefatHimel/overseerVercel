c0322fd263607cceffc6750205a120af06870369
SeefatHimel <Seefat.hossain@brainstation-23.com>
2023-02-20 18:42:24 +0600

    fix build

--- config/index.ts
+++ config/index.ts
@@ -4,7 +4,3 @@ export const config = {
   process?.env.NEXT_PUBLIC_API_PREFIX_GRAPHQL || "http://localhost:3000/graphql",
   restPrefix: process?.env.NEXT_PUBLIC_API_PREFIX_REST,
 };
-console.log(
-  "🚀 ~ file: index.ts:13 ~ NEXT_PUBLIC_REST_API_PREFIX_REST",
-  process?.env.NEXT_PUBLIC_API_PREFIX_REST
-);
